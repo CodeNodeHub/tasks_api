ccfa48ef46be2bfaaa1eb6c6e611f34326226086
SWillames <sergiowoc@gmail.com>
2024-09-17 05:35:00 -0300

    Separating routes from the application.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import http from 'node:http'
-import { Database } from './database.js'
 import { json } from './middlewares/json.js'
+import { route, routes } from './routes.js'
 
 const database = new Database()
 
@@ -9,25 +9,12 @@ const server = http.createServer(async (req, res) => {
 
   await json(req, res)
 
-  if (method === 'GET' && url === '/tasks'){
-    
-    const tasks = database.select('tasks')
-    
-    return res.end(JSON.stringify(tasks))
-  }
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  })
 
-  if (method === 'POST' && url === '/tasks') {
-    const { title, description } = req.body
-    const task = {
-      id: 1,
-      title,
-      description,
-      completed_at: null
-    }
-
-    database.insert('tasks', task)
-
-    return res.writeHead(201).end()
+  if (route) {
+    return route.handler(req, res)
   }
 
   return res.writeHead(404).end()
