44a2c8492454979b87dec6576b5438a6f04edffc
SWillames <sergiowoc@gmail.com>
2024-09-17 03:50:00 -0300

    Adding HTTP Status Codes.

--- src/server.js
+++ src/server.js
@@ -1,17 +1,28 @@
 import http from 'node:http'
 
+const tasks = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === 'GET' && url === '/tasks'){
-    return res.end('Listagem de tarefas')
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(tasks))
   }
 
-  if (method === 'POST' && url === 'tasks') {
-    return res.end('Criação de tasks')
+  if (method === 'POST' && url === '/tasks') {
+    tasks.push({
+      id: 1,
+      title: "Estudar Node",
+      description: "Aprenda o básico do Node.js",
+      completed_at: null
+    })
+
+    return res.writeHead(201).end()
   }
 
-  return res.end('Hello World')
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
